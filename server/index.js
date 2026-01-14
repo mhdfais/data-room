acaa6a006bf980c1fdbf3ae38be522578feca4ec
Mohammed Fais TM <mhdfaistm@gmail.com>
2026-01-14 16:15:39 +0530

    fallback for cores

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || "https://data-room-frontend.up.railway.app",
   credentials: true
 }));
 
