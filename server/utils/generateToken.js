deb31ddbd3be051b5ca12cdb3877bce5e71ad06e
Mohammed Fais TM <mhdfaistm@gmail.com>
2026-01-14 16:38:46 +0530

    cookie issue

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 };
